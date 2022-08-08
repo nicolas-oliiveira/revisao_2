cd9db481d59a81ca022dc500a3c53238dd0c6187
nicolas-oliiveira <nicolas.oliveira115@etec.sp.gov.br>
2022-08-08 11:06:22 -0300

    oloquinho meu

---
+++ app/src/main/java/com/example/revisao/Resultado.kt
@@ -0,0 +1,29 @@
+package com.example.revisao
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class Resultado : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_resultado)
+
+        val intent = intent
+        val inputNome = intent.getStringExtra("nome")
+
+        
+
+        val btnVoltar: Button = findViewById(R.id.btnVoltar)
+
+
+        btnVoltar.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+
+        }
+    }
+}
